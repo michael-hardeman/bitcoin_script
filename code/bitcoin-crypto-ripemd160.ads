f7fbe594bd2f23e7f4a9a99e813db660dc3e4841
Michael Hardeman <michael.hardeman@protonmail.com>
2017-11-08 18:25:33 -0500

    Ripemd 160 Support

--- code/bitcoin-crypto-ripemd160.ads
+++ code/bitcoin-crypto-ripemd160.ads
@@ -1,2 +1,6 @@
+with Bitcoin.API.OpenSSL;
+
 package Bitcoin.Crypto.RIPEMD160 is
+  function Digest (B : in Byte_Array) return Byte_Array;
+  function Digest (S : in String)     return Byte_Array is (Digest (To_Byte_Array (S)));
 end;
