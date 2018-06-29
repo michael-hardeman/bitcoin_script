5d9aff8613fc86a5d79ef71fb73684066923d510
Michael Hardeman <michael.hardeman@protonmail.com>
2018-06-29 18:00:54 -0400

    Implementing base64 tests

--- tests/bitcoin-encoding-base_64_tests.adb
+++ tests/bitcoin-encoding-base_64_tests.adb
@@ -9,10 +9,6 @@ with Bitcoin.Test_Utilities.Common; use Bitcoin.Test_Utilities.Common;
 
 package body Bitcoin.Encoding.Base_64_Tests is
 
-  ---------------
-  -- Constants --
-  ---------------
-
   --------------------
   -- Register_Tests --
   --------------------
@@ -28,7 +24,9 @@ package body Bitcoin.Encoding.Base_64_Tests is
   -----------------
   procedure Test_Encode (Test : in out Test_Cases.Test_Case'Class) is
   begin
-    null;
+    Assert_Strings_Equal (Expected => "YW55IGNhcm5hbCBwbGVhc3Vy", Actual => To_String (Encode (To_Byte_Array ("any carnal pleasur"))));
+    Assert_Strings_Equal (Expected => "YW55IGNhcm5hbCBwbGVhc3U=", Actual => To_String (Encode (To_Byte_Array ("any carnal pleasu"))));
+    Assert_Strings_Equal (Expected => "YW55IGNhcm5hbCBwbGVhcw==", Actual => To_String (Encode (To_Byte_Array ("any carnal pleas"))));
   end;
 
   -----------------
@@ -36,7 +34,9 @@ package body Bitcoin.Encoding.Base_64_Tests is
   -----------------
   procedure Test_Decode (Test : in out Test_Cases.Test_Case'Class) is
   begin
-    null;
+    Assert_Strings_Equal (Expected => "any carnal pleasur", Actual => To_String (Decode ("YW55IGNhcm5hbCBwbGVhc3Vy")));
+    Assert_Strings_Equal (Expected => "any carnal pleasu",  Actual => To_String (Decode ("YW55IGNhcm5hbCBwbGVhc3U=")));
+    Assert_Strings_Equal (Expected => "any carnal pleas",   Actual => To_String (Decode ("YW55IGNhcm5hbCBwbGVhcw==")));
   end;
 
 end;
