cf6a8f562b4ae98e080f2d041d2ccd2f7e0a1ddf
Michael Hardeman <michael.hardeman@protonmail.com>
2018-06-30 00:27:51 -0400

    Implement the RIPEMD160 tests

--- tests/bitcoin-crypto-ripemd160_tests.adb
+++ tests/bitcoin-crypto-ripemd160_tests.adb
@@ -9,10 +9,6 @@ with Bitcoin.Test_Utilities.Common; use Bitcoin.Test_Utilities.Common;
 
 package body Bitcoin.Crypto.RIPEMD160_Tests is
 
-  ---------------
-  -- Constants --
-  ---------------
-
   --------------------
   -- Register_Tests --
   --------------------
@@ -27,7 +23,35 @@ package body Bitcoin.Crypto.RIPEMD160_Tests is
   -----------------
   procedure Test_Digest (Test : in out Test_Cases.Test_Case'Class) is
   begin
-    null;
+    Assert_Byte_Arrays_Equal (
+      Expected => (
+        16#7c#, 16#03#, 16#82#, 16#0b#, 16#46#,
+        16#d6#, 16#b5#, 16#ba#, 16#0e#, 16#f5#,
+        16#c5#, 16#d0#, 16#90#, 16#83#, 16#8f#,
+        16#6e#, 16#53#, 16#ec#, 16#84#, 16#63#),
+      Actual => Digest ("Jackdaws love my big sphinx of quartz."));
+
+    Assert_Byte_Arrays_Equal (
+      Expected => (
+        16#1e#, 16#d9#, 16#1d#, 16#d9#, 16#08#,
+        16#46#, 16#95#, 16#f4#, 16#7f#, 16#68#,
+        16#db#, 16#e7#, 16#2b#, 16#a4#, 16#56#,
+        16#f2#, 16#be#, 16#36#, 16#b3#, 16#9b#),
+      Actual => Digest (
+        "A purely peer-to-peer version of electronic cash would allow "  &
+        "online payments to be sent directly from one party to another " &
+        "without going through a financial institution."));
+
+    Assert_Byte_Arrays_Equal (
+      Expected => (
+        16#fe#, 16#73#, 16#e6#, 16#26#, 16#7f#,
+        16#22#, 16#3a#, 16#51#, 16#01#, 16#44#,
+        16#8c#, 16#5c#, 16#b1#, 16#18#, 16#7e#,
+        16#0f#, 16#2d#, 16#45#, 16#5b#, 16#aa#),
+      Actual => Digest (
+        "Ada was originally designed with three overriding concerns: program reliability " &
+        "and maintenance, programming as a human activity, and efficiency."));
+
   end;
 
 end;
