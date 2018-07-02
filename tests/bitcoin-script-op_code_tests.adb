e5597d28307c09bcea980e14ddf18d93c8ca63ea
Michael Hardeman <michael.hardeman@protonmail.com>
2018-07-02 13:09:45 -0400

    implementing the first op_code test

--- tests/bitcoin-script-op_code_tests.adb
+++ tests/bitcoin-script-op_code_tests.adb
@@ -22,10 +22,14 @@ package body Bitcoin.Script.Op_Code_Tests is
   -- Test_OP_0 --
   ---------------
   procedure Test_OP_0 (Test : in out Test_Cases.Test_Case'Class) is
+    use Byte_Array_Stacks;
+
     Script          : constant Byte_Array := (1 => To_Byte (OP_0));
-    Primary_Stack   : Byte_Array_Stacks.Stack_Type;
-    Secondary_Stack : Byte_Array_Stacks.Stack_Type;
+    Primary_Stack   :          Stack_Type;
+    Secondary_Stack :          Stack_Type;
   begin
     Evaluate (Script, Primary_Stack, Secondary_Stack);
+
+    Assert_Byte_Arrays_Equal (Expected => (1 .. 4 => 16#00#), Actual => Get (Primary_Stack, 1));
   end;
 end;
